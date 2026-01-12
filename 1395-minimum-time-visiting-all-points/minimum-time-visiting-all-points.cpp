c287ee6e5c9589e6ca67656369b0f28fa58b262f
Vibha-1802 <168212937+Vibha-1802@users.noreply.github.com>
2026-01-12 23:52:12 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (61.26%) - LeetSync

---
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int sec=0;
+        for(int i=0;i<points.size()-1;i++)
+        {
+            int x1=points[i][0];
+            int y1=points[i][1];
+            int x2=points[i+1][0];
+            int y2=points[i+1][1]; 
+            int dx=abs(x2-x1);
+            int dy=abs(y2-y1);
+            int diagonal=min(dx,dy);
+            int remaining=abs(dx-dy);
+            sec=sec+diagonal+remaining;
+        }
+        return sec;
+    }
+};
