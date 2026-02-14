a346bf501ac37a220faa4185e9c3ffd1ad70f6a9
Vibha-1802 <168212937+Vibha-1802@users.noreply.github.com>
2026-02-14 23:30:31 +0530

    Time: 17 ms (8.72%) | Memory: 45.7 MB (14.53%) - LeetSync

---
+++ 815-champagne-tower/champagne-tower.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+        vector<vector<double>> t(101,vector<double>(101,0.0));
+        t[0][0]=(double)poured;      
+        for(int row= 0;row<=query_row;row++) 
+            for(int col=0;col<=row;col++) {
+                double excess=(t[row][col]-1.0)/2.0;
+                if(excess > 0) { 
+                    t[row+1][col] += excess;
+                    t[row+1][col+1] += excess;
+                }
+            }
+        return min(1.0,t[query_row][query_glass]);
+    }
+};
