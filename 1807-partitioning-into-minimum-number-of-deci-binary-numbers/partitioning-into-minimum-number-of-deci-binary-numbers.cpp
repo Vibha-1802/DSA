b4f8e7e5e626bdf1cf223be2a4246f107e35479c
Vibha-1802 <168212937+Vibha-1802@users.noreply.github.com>
2026-03-01 22:52:07 +0530

    Time: 20 ms (5.68%) | Memory: 16.6 MB (16.53%) - LeetSync

---
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minPartitions(string n) {
+        int count=0;
+        while (true) {
+            bool changed=false;
+            for (char &ch:n) {
+                if (ch!='0') {
+                    ch--;  
+                    changed=true;
+                }
+            }
+            if (!changed)
+                break;
+            count++;
+        }
+        return count;
+    }
+};
